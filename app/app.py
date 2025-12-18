d7a7d3bea2b741c3960d3a653b0c4b686e4299d1
thung4974-a11y <thung4974@gmail.com>
2025-12-19 01:51:05 +0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -33,12 +33,13 @@ def main():
         login_page(conn)
     else:
         if st.session_state.get('role') == 'teacher':
-            teacher_dashboard(connconn, df)
+            teacher_dashboard(conn, df)
         else:
             student_dashboard(conn)
 
 if __name__ == "__main__":
     main()
+
 
 
 
